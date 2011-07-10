44fd08ffb40817ab66f14c0403f97ed002514d53
Aleksander Ambrozkiewicz <a.ambrozkiewicz@gmail.com>
2011-07-10 21:14:37 +0200

    initial

---
+++ Blueprint/BlueprintForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Blueprint
+{
+    partial class BlueprintForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.basePathLbl = new System.Windows.Forms.Label();
+            this.basePathDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.structureTxt = new System.Windows.Forms.TextBox();
+            this.basePathBtn = new System.Windows.Forms.Button();
+            this.generateBtn = new System.Windows.Forms.Button();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.quitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // basePathLbl
+            // 
+            this.basePathLbl.AutoSize = true;
+            this.basePathLbl.Location = new System.Drawing.Point(155, 38);
+            this.basePathLbl.Name = "basePathLbl";
+            this.basePathLbl.Size = new System.Drawing.Size(148, 13);
+            this.basePathLbl.TabIndex = 0;
+            this.basePathLbl.Text = "Select base path using button";
+            // 
+            // structureTxt
+            // 
+            this.structureTxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.structureTxt.Location = new System.Drawing.Point(12, 62);
+            this.structureTxt.Multiline = true;
+            this.structureTxt.Name = "structureTxt";
+            this.structureTxt.Size = new System.Drawing.Size(508, 318);
+            this.structureTxt.TabIndex = 1;
+            // 
+            // basePathBtn
+            // 
+            this.basePathBtn.Location = new System.Drawing.Point(12, 33);
+            this.basePathBtn.Name = "basePathBtn";
+            this.basePathBtn.Size = new System.Drawing.Size(137, 23);
+            this.basePathBtn.TabIndex = 2;
+            this.basePathBtn.Text = "Choose base path";
+            this.basePathBtn.UseVisualStyleBackColor = true;
+            this.basePathBtn.Click += new System.EventHandler(this.basePathBtn_Click);
+            // 
+            // generateBtn
+            // 
+            this.generateBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.generateBtn.Enabled = false;
+            this.generateBtn.Location = new System.Drawing.Point(445, 386);
+            this.generateBtn.Name = "generateBtn";
+            this.generateBtn.Size = new System.Drawing.Size(75, 23);
+            this.generateBtn.TabIndex = 3;
+            this.generateBtn.Text = "Generate";
+            this.generateBtn.UseVisualStyleBackColor = true;
+            this.generateBtn.Click += new System.EventHandler(this.generateBtn_Click);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip.Size = new System.Drawing.Size(532, 24);
+            this.menuStrip.TabIndex = 4;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openMenuItem,
+            this.saveMenuItem,
+            this.toolStripSeparator1,
+            this.quitMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openMenuItem
+            // 
+            this.openMenuItem.Name = "openMenuItem";
+            this.openMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openMenuItem.Text = "Open";
+            this.openMenuItem.Click += new System.EventHandler(this.openMenuItem_Click);
+            // 
+            // saveMenuItem
+            // 
+            this.saveMenuItem.Name = "saveMenuItem";
+            this.saveMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveMenuItem.Text = "Save";
+            this.saveMenuItem.Click += new System.EventHandler(this.saveMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // quitMenuItem
+            // 
+            this.quitMenuItem.Name = "quitMenuItem";
+            this.quitMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.quitMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.quitMenuItem.Text = "Quit";
+            this.quitMenuItem.Click += new System.EventHandler(this.quitMenuItem_Click);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "txt";
+            this.saveFileDialog.Filter = "Text file|*.txt";
+            // 
+            // BlueprintForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 421);
+            this.Controls.Add(this.generateBtn);
+            this.Controls.Add(this.basePathBtn);
+            this.Controls.Add(this.structureTxt);
+            this.Controls.Add(this.basePathLbl);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "BlueprintForm";
+            this.Text = "Blueprint";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label basePathLbl;
+        private System.Windows.Forms.FolderBrowserDialog basePathDialog;
+        private System.Windows.Forms.TextBox structureTxt;
+        private System.Windows.Forms.Button basePathBtn;
+        private System.Windows.Forms.Button generateBtn;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem quitMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ Blueprint/BlueprintForm.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Blueprint
+{
+    public partial class BlueprintForm : Form
+    {
+        protected readonly Regex re = new Regex(@"^(/?)([\w\.]+/)*([\w\.]+)?(?::([\w]+))?$");
+
+        public BlueprintForm()
+        {
+            InitializeComponent();
+        }
+
+        private void basePathBtn_Click(object sender, EventArgs e)
+        {
+            if (basePathDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                basePathLbl.Text = basePathDialog.SelectedPath;
+                generateBtn.Enabled = true;
+            }
+        }
+
+        private void quitMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void generateBtn_Click(object sender, EventArgs e)
+        {
+            string basePath = this.basePathDialog.SelectedPath + "/",
+                   path = basePath;
+
+            foreach (string line in structureTxt.Lines)
+            {
+                GroupCollection groups = re.Match(line).Groups;
+
+                if (groups[1].Value == "/")
+                {
+                    path = basePath;
+                    foreach (Capture capt in groups[2].Captures)
+                        path += capt;
+                }
+                else if (groups[2].Captures.Count > 0)
+                {
+                    foreach (Capture capt in groups[2].Captures)
+                        path += capt;
+                }
+
+                if (groups[3].ToString() != String.Empty) // file
+                {
+                    try
+                    {
+                        StreamWriter writer = File.CreateText(path + groups[3]);
+                        if (groups[4].ToString() != String.Empty)
+                        {
+                            string blueprintName = groups[4].ToString();
+
+                            try
+                            {
+                                StreamReader blueprint = new StreamReader(Application.StartupPath + "/blueprints/" + blueprintName + ".txt");
+                                writer.Write(blueprint.ReadToEnd());
+                                blueprint.Close();
+                            }
+                            catch (FileNotFoundException)
+                            {
+                                MessageBox.Show("Blueprint " + blueprintName + " not found", "Blueprint");
+                            }
+                        }
+                        writer.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else // directory
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+
+            MessageBox.Show("Blueprint created!", "Blueprint");
+        }
+
+        private void saveMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamWriter writer = new StreamWriter(saveFileDialog.FileName);
+                foreach (string line in structureTxt.Lines)
+                    writer.WriteLine(line);
+                writer.Close();
+            }
+        }
+
+        private void openMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader reader = new StreamReader(openFileDialog.FileName);
+                structureTxt.Text = reader.ReadToEnd();
+            }
+        }
+    }
+}
